5cb0212abbf761b795b5f2437ce6e42502268d64
Nelson <g00412122@atu.ie>
2023-10-06 09:38:29 +0000

    code restructu

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="Create">Create</Nav.Link>
             <Nav.Link href="Read">Read</Nav.Link>
           </Nav>
