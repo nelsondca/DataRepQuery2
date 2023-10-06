79b7033139f1f017a2d67c2772685672e0742ab4
Nelson <g00412122@atu.ie>
2023-10-06 08:44:19 +0000

    Lab2 START

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+function Header(){
+//component    
+return(
+    <div>
+        <h2>Hello from another component</h2>
+    </div>
+);// writing the visual piece of this component
+}
+
+export default Header;
