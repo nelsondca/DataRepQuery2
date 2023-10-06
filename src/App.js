5e8c345a7d173b595a40d5030600aeb3cc1f0da8
Nelson <g00412122@atu.ie>
2023-10-06 08:48:29 +0000

    3(b) time under hello

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Header from './components/header';
 function App() {
   return (
     <div className ="App">
-      {/* <Header></Header> */}
+        <Header></Header>
       <h1>Hello world!</h1>
-      {/* <h2>It is {new Date().toLocaleTimeString()}.</h2> */}
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
    );
 }
 
-// export default Header;
+export default App;
