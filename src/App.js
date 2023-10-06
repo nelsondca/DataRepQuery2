d520f607ac95c5e1c71e08112d5173ab528a62c3
Nelson <g00412122@atu.ie>
2023-10-06 09:32:31 +0000

    5(a)(b) bootsrap instal and router dom

--- src/App.js
+++ src/App.js
@@ -3,16 +3,43 @@ import './App.css';
 import Header from './components/header';
 import Content from './components/content';
 import Footer from  './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+
+
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div className ="App">
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="Create">Create</Nav.Link>
+            <Nav.Link href="Read">Read</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      {/* Everything between here is going to change */}
+      <Routes>
+        <Route path='/' element = {<Content></Content>}></Route>
+        <Route path='/read' element = {<Header></Header>}></Route>
+        <Route path='/create' element = {<Footer></Footer>}></Route>
+      </Routes>
+
         {/* Calling the functions from another components */}
-        <Header></Header>
+        {/* <Header></Header>
         <Content></Content>
-        <Footer></Footer>
+        <Footer></Footer> */}
     </div>
+    </BrowserRouter>
    );
 }
 
